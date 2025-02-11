d7ff4eea38a65b2ea3b8c8e71a40b66cbb0c3ec1
Suke-H <hondii1997@gmail.com>
2025-02-11 23:48:37 +0900

    ✨ feat: #46 Playモードをレスポンシブ対応

--- frontend/src/components/play-page.tsx
+++ frontend/src/components/play-page.tsx
@@ -8,16 +8,13 @@ import { PanelList } from '@/components/editor/panel-list';
 const PlayPage: React.FC = () => {
   const dispatch = useDispatch();
 
-  // 毎回プレイモード起動時に、グリッド履歴をクリアする
-  // (課題)エディタ切替時も、操作をリセットする必要がある
-  // // （clearHistory:履歴を空に、reset:履歴を空に・グリッドを最初の状態に）
   useEffect(() => {
     dispatch(gridSlice.actions.clearHistory());
   }, [dispatch]);
 
   return (
-    <div className="flex flex-col p-4 gap-4 min-h-screen">
-      <div className="flex gap-4">
+    <div className="flex flex-col gap-4 min-h-screen">
+      <div className="flex gap-4 flex-col md:flex-row">
         <Grid />
         <PanelList />
       </div>
