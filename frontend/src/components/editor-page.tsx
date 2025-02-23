a2d87590196ca9932c8c4b26aa67024a5c82965b
Suke-H <hondii1997@gmail.com>
2025-02-23 11:31:20 +0900

    ✅ fix: Playモードでパネルを設置していた際、モード切替時にリセットするように修正

--- frontend/src/components/editor-page.tsx
+++ frontend/src/components/editor-page.tsx
@@ -2,6 +2,7 @@ import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 
 import { gridSlice } from '../store/slices/grid-slice';
+import { panelListSlice } from '../store/slices/panel-list-slice';
 
 import { CellTypeSelector } from '@/components/editor/cell-type-selector';
 import { Grid } from '@/components/editor/grid';
@@ -14,6 +15,8 @@ const EditorPage: React.FC = () => {
   // エディタモード時に、プレイモードで実施したパネル配置をリセット
   useEffect(() => {
     dispatch(gridSlice.actions.reset());
+    dispatch(panelListSlice.actions.reset());
+
   }, [dispatch]);
 
   return (
