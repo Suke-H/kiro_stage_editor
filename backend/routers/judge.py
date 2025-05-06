2485b83c7fdb75bea9135272f48ffc707b441367
Suke-H <hondii1997@gmail.com>
2025-05-06 13:58:14 +0900

    💚 fix: #62 modelをpydantic v2に合わせることで解決
    close #69

--- backend/routers/judge.py
+++ backend/routers/judge.py
@@ -1,7 +1,7 @@
 import pydantic
-import pydantic.v1
-pydantic.BaseModel = pydantic.v1.BaseModel
-pydantic.Extra     = pydantic.v1.Extra
+# import pydantic.v1
+# pydantic.BaseModel = pydantic.v1.BaseModel
+# pydantic.Extra     = pydantic.v1.Extra
 
 from fastapi import APIRouter
 from fastapi import Request, Body
@@ -13,13 +13,10 @@ router = APIRouter()
 
 @router.post("/judge")
 async def judge_check(grid: Grid):
-    # raw = await request.body()
-    # print(f"Raw request body: {raw!r}")
-
     print(f"Grid: {grid}")
 
     example = PathResult(
-        path=Path(__root__=[
+        path=Path([
             Vector(x=0, y=0),
             Vector(x=1, y=1),
             Vector(x=2, y=2),
